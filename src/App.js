e9c4225410c5b848e3d251a8a39ef634ff361ecb
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2020-12-11 16:24:59 +0530

    Commit before deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
   "Bangalore",
   "Delhi",
   "Mumbai",
-  "Chennai"]
+  "Chennai",
+  "Ahmedabad"]
     
   
   function errorHandler(error)
@@ -33,7 +34,7 @@ function App() {
       // setLong(coordinates.lon)
       lattitude=coordinates.lat;
       longitude=coordinates.lon;
-      var urlToPlaces=`https://api.opentripmap.com/0.1/en/places/radius?apikey=5ae2e3f221c38a28845f05b60e7759b29878873a69598c9d75b72fbe&radius=10000&limit=50&offset=0&lon=${longitude}&lat=${lattitude}&rate=2&format=json` 
+      var urlToPlaces=`https://api.opentripmap.com/0.1/en/places/radius?apikey=5ae2e3f221c38a28845f05b60e7759b29878873a69598c9d75b72fbe&radius=10000&limit=100&offset=0&lon=${longitude}&lat=${lattitude}&rate=2&format=json` 
       fetch(urlToPlaces)
       .then(response=>response.json())
       .then(placeData=>setPlaces(placeData))
@@ -51,6 +52,7 @@ function App() {
       // setLong(coordinates.lon)
       lattitude=coordinates.lat;
       longitude=coordinates.lon;
+      setLocation("")
       var urlToPlaces=`https://api.opentripmap.com/0.1/en/places/radius?apikey=5ae2e3f221c38a28845f05b60e7759b29878873a69598c9d75b72fbe&radius=10000&limit=50&offset=0&lon=${longitude}&lat=${lattitude}&rate=2&format=json` 
       fetch(urlToPlaces)
       .then(response=>response.json())
@@ -64,10 +66,31 @@ function App() {
     inputLocation=event.target.value
     setLocation(inputLocation)
   }
+
+  const placeCard=places.length && places.map(place=>{
+    var urltoMap="https://maps.google.com?q="+place.point.lat+","+place.point.lon;
+    return(
+    <div class="card">
+    <div class="container">
+    <h3><b>{place.name}</b></h3>
+    <h4>Rated {place.rate}</h4>
+    <h5>Aproxx {(place.dist/1000).toFixed(2)} kms from City Centre</h5>
+    <button className="Buttonrownav"
+    ><a target="_blank" href={urltoMap}>Open in Google Maps</a></button>
+    <h5></h5>
+    <h5></h5>
+    </div>
+    </div>
+)});
+    
+    
+  
+
   return (
     <div className="App">
       <div className="App-header">
       <h1 className="Heading">goodTravel</h1>
+      <h1 className="Heading"> </h1>
       <div className="search">
       <input 
       value={location}
@@ -79,14 +102,23 @@ function App() {
       onClick={clickHandler}
       >GO</button>
       </div>
-      <h1 className="Heading">{location}</h1>
+      <h1 className="Heading"> </h1>
       <div >
       {commomCity.map(city=><button className="Buttonrow"
       onClick={()=>commoncityclickHandler(city)}
       >{city}</button>)}
       </div>
 
-      {(places.length)?(places.map(place=><h6>{place.name}</h6>)):"Attractions of your city would appear here if available ! "}
+      {(places.length)? placeCard :(
+        <div class="cardhigh">
+        <div class="container">
+        <h1 className="Heading"> </h1>
+        <h3><b>Tourist Attractions of your city would appear here if available ! </b></h3>
+        <p>We are still exploring your city if the attractions don't appear on Search.</p>
+        <h1 className="Heading"> </h1>
+        </div>
+        </div>
+        )}
       </div>
     </div>
   );
